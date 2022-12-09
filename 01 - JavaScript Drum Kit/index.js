185bda88b75f0120e9295798bc71ce1c8af1c7c9
Spencer Penrod <spencerpenrod@gmail.com>
2022-12-08 20:35:50 -0700

    Update deprecated keycodes to use key.code instead

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,14 +1,13 @@
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio)
-      return;
+  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  const key = document.querySelector(`.key[data-key="${e.code}"]`);
+  if (!audio) return;
 
-    audio.currentTime = 0;
-    audio.play();
+  audio.currentTime = 0;
+  audio.play();
 
-    key.classList.add('playing');
-};
+  key.classList.add('playing');
+}
 
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return;
@@ -18,5 +17,5 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
-  
+
 window.addEventListener('keydown', playSound);
